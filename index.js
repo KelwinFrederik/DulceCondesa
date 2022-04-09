c3f9fd68b177fa68d3631cb84c48e2af6840d4f6
Kelwin F. Alves <kelwin.frederik@gmail.com>
2022-04-09 16:57:33 -0300

    Update - Ajustes finais Responsividade - Inicio criação popUp

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 //TODO: alterar titulo, mainPhoto e MainInformation dinamicamente
 //document.tilte = "TATA"
 //TODO: carregar catálogo dinamicamente
+
+function openPopUp(sabor){
+    popUpWindow = window.open('popUpSabores.html','popup')
+}
