6c3446895fe05246b2a526b6ffe0afc05e40e606
Kelwin F. Alves <kelwin.frederik@gmail.com>
2022-04-12 23:11:58 -0300

    Update - Criação de funcionalidade e aparencia PopUp Sabores

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-
 //TODO: alterar titulo, mainPhoto e MainInformation dinamicamente
 //document.tilte = "TATA"
 //TODO: carregar catálogo dinamicamente
+const OpenSaborButton = document.querySelectorAll('[data-button-Sabores]')
+const closeSaboresButton = document.querySelectorAll('[data-button-Close]')
+const Overlay = document.getElementById('overlay')
 
-function openPopUp(sabor){
-    popUpWindow = window.open('popUpSabores.html','popup')
+//EVENTS
+OpenSaborButton.forEach( button => {
+    button.addEventListener('click', () =>{
+        const sabores = document.querySelector(button.dataset.buttonSabores)
+        openPopUp(sabores, button.id)
+    });
+});
+
+closeSaboresButton.forEach( button => {
+    button.addEventListener('click', () =>{
+        const sabores = button.closest('#Sabores')
+        closePopUp(sabores)
+    })
+})
+
+//FUNCTIONS
+function openPopUp(sabor, tipo){
+    if(sabor == null) return;
+    if(Overlay == null) return;
+    
+    editarLegendPopUp(tipo);
+
+    sabor.classList.add('active')
+    Overlay.classList.add('active')
+}
+
+function editarLegendPopUp(tipo){
+    let legend = "<i class='fa-solid fa-spoon'></i> Sabores " + tipo
+    document.getElementById('SaboresTitulo').innerHTML = legend
+}
+
+function closePopUp(sabor){
+    if(sabor == null) return;
+    if(Overlay == null) return;
+
+    sabor.classList.remove('active')
+    Overlay.classList.remove('active')
 }
